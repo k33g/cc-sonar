f584ceaef22cfcadfd3af0eadd2dbf0c08767338
Philippe Charrière <ph.charriere@gmail.com>
2017-09-29 16:12:35 +0200

    3

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ cd cc-starter
 mvn compile assembly:single
 mv target/starter.1.0-SNAPSHOT.jar ../starter.1.0-SNAPSHOT.jar
 cd ..
-echo "👋 SonarQube setup and deployment is started"
+echo "👋 🌍 SonarQube setup and deployment is started"
 curl -L https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.5.zip -o sonarqube-6.5.zip
 tar zxvf sonarqube-6.5.zip
 mv sonarqube-6.5/conf/sonar.properties sonarqube-6.5/conf/sonar.properties.old
 
 # === sonar.properties file ===
 cat > sonarqube-6.5/conf/sonar.properties << EOF
-sonar.web.port=80
+sonar.web.port=8080
 EOF
