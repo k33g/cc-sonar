50ce82b317a3d0be415374971f0696508e17c4ad
Philippe Charrière <ph.charriere@gmail.com>
2017-09-29 16:32:22 +0200

    6

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ mv target/starter.1.0-SNAPSHOT.jar ../starter.1.0-SNAPSHOT.jar
 cd ..
 echo "👋 🌍 🤔 SonarQube setup and deployment is started"
 curl -L https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.5.zip -o sonarqube-6.5.zip
-#tar zxvf sonarqube-6.5.zip
 unzip sonarqube-6.5.zip
 
 mv sonarqube-6.5/conf/sonar.properties sonarqube-6.5/conf/sonar.properties.old
